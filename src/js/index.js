f2a05b464da07a63c8a86aa21520389210ee8803
Great_MaxKyr <maxkyrylyuk@gmail.com>
2023-03-11 08:06:15 +0000

    pages

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,10 @@ hamburger.onclick = function () {
     navigatemenu.classList.toggle("active-burger")
     hamburger.classList.toggle("hamburger-open")
 }
+
+let black = document.querySelector(".black")
+let bottommenu = document.querySelector(".bottommenu")
+black.onclick = function () {
+    navigatemenu.classList.toggle("active-burger")
+    black.classList.toggle("hamburger-open")
+}
